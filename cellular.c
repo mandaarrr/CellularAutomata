c78ff7b6a203b1d6c3789a9400737b72e6ab3174
ArchieRutherford <archiechalmers@gmail.com>
2018-11-01 13:29:27 +0000

    It actually works!

--- cellular.c
+++ cellular.c
@@ -5,9 +5,9 @@
 void main()
 {
 	int ruleInInt, width, height;
-	
+
 	printf("Cellular Automaton");
-	
+
 	printf("Enter the Rule: ");
 	scanf("%d", &ruleInInt);
 
@@ -20,109 +20,65 @@ void main()
 		ruleInInt /= 2;
 		temp++;
 	}
-	
-	
+
 	printf("Enter the width of the grid: ");
 	scanf("%d", &width);
-	
+
 	printf("Enter the height of the grid: ");
 	scanf("%d", &height);
-	
+
 	int numArray[height][width];
 
 	for (int i = 0; i < height; i++)
 		for(int j=0; j<width; j++)
 			numArray[i][j]=0;
-	
+
 	numArray[0][width/2] = 1;
 
- 
+
 //Looping and skipping the edge cells
-for (int i = 1; i < height-1; i++)
+for (int i = 1; i < height; i++)
   {
-  	for (int j = 1; j < width-1; j++)
+  	for (int j = 0; j < width; j++)
  	 {
 	//Add up all the neighbor states to calculate the number of neighbors.
-    	int pattern[3];
-    	pattern[0] = numArray[i-1][j-1];
-    	pattern[1] = numArray[i-1][j];
-    	pattern[2] = numArray[i-1][j+1];
 
-    	int patternInt = (pattern[0]*100 + pattern[1]*10 + pattern[2]);
+			int patternInt = 0;
 
-    	switch(patternInt)
-    	{
-			case 000:
-			if (binaryArray[7] == 0)
+			if (j==width)
+			{
+				if (numArray[i-1][j-1] == 0)
+					patternInt += 4;
+				if (numArray[i-1][j] == 0)
+					patternInt += 2;
+
+				patternInt += 1;
+			}
+			else if (j==0)
+			{
+				patternInt += 4;
+
+				if (numArray[i-1][j] == 0)
+					patternInt += 2;
+				if (numArray[i-1][j+1] == 0)
+					patternInt += 1;
+			}
+			else
+			{
+				if (numArray[i-1][j-1] == 0)
+					patternInt += 4;
+				if (numArray[i-1][j] == 0)
+					patternInt += 2;
+				if (numArray[i-1][j+1] == 0)
+					patternInt += 1;
+				}
+
+			if (binaryArray[patternInt] == 0)
+				numArray[i][j] = 0;
+			else if (binaryArray[patternInt] == 1)
 				numArray[i][j] = 1;
-			else if (binaryArray[7] == 1)
-				numArray[i][j] = 1;
-
-			case 001:
-			if (binaryArray[6] == 0)
-				numArray[i][j] =  0;
-			else if (binaryArray[6] == 1)
-				numArray[i][j] =  1;
-
-			case 010:
-			if (binaryArray[5] == 0)
-				numArray[i][j] =  0;
-			else if (binaryArray[5] ==1)
-				numArray[i][j] =  1;
-
-			case 011:
-			if (binaryArray[4] == 0)
-				numArray[i][j] =  0;
-			else if (binaryArray[4] == 1)
-				numArray[i][j] =  1;
-
-			case 100:
-			if (binaryArray[3] == 0)
-				numArray[i][j] =  0;
-			else if (binaryArray[3] == 1)
-				numArray[i][j] =  1;
-
-			case 101:
-			if (binaryArray[2] == 0)
-				numArray[i][j] =  0;
-			else if (binaryArray[2] == 1)
-				numArray[i][j] =  1;
-
-			case 110:
-			if (binaryArray[1] == 0)
-				numArray[i][j] =  0;
-			else if (binaryArray[1] == 1)
-				numArray[i][j] =  1;
-
-			case 111:
-			if (binaryArray[0] == 0)
-				numArray[i][j] =  0;
-			else if (binaryArray[0] == 1)
-				numArray[i][j] =  1;
 		}
 	}
-}
-
-
-
-
-
-	
-	for(int i=0; i<height; i++)
-	{
-		for(int j=0; j<width; j++)
-		{
-			printf("%d ",numArray[i][j]);
-		}
-		printf("\n");
-	}
-
-
-
-
-
-
-
 
 	for(int i=0; i<height; i++)
 	{
@@ -131,7 +87,7 @@ for (int i = 1; i < height-1; i++)
 			if (numArray[i][j] == 1)
 				printf("\u25A0 ");
 			else if (numArray[i][j] == 0)
-				printf("\u25A1 ");
+				printf("  ");
 		}
 		printf("\n");
 	}
